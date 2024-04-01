df7ea2c20b2e30d9c7cef5aafc327c2187cbc779
omeher26 <onkarmeher500b@gmail.com>
2024-04-01 14:16:15 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{color:'green'}}>Hello Onkar Meher Genious 1cr package</Text>
+    </View>
+  );
+};
+
+export default App
+
+
+
